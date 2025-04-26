f147286e7c010f9ad8a17533b4a6ce0dd01a7b8f
officialODBIT <contact@odbit.org>
2025-04-26 14:40:31 -0400

    Update ban.js

--- api/ban.js
+++ api/ban.js
@@ -1,16 +1,9 @@
-// api/ban.js
 module.exports = async (req, res) => {
-  // Get the user's IP address from the request
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const bannedIps = ['', ''];
 
-  // List of banned IPs (for testing, you can hardcode some)
-  const bannedIps = ['172.56.220.2', '98.76.54.32'];
-
-  // Check if the IP is banned
   if (bannedIps.includes(ip)) {
     return res.status(403).json({ message: 'Your IP is banned.' });
   }
-
-  // If the IP is not banned
-  return res.status(200).json({ message: 'Welcome!' });
+  return res.status(200).json({ message: '!codeword: eagle' });
 };
